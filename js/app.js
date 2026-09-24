c11872124d6752e5e8c36000ce540a418fbd1fcf
rukhiya12345786-cpu <rukhiya12345786@gmail.com>
2026-09-25 00:24:54 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,60 @@
 alert("APP JS IS LOADING");
+const video = document.getElementById("camera");
+const status = document.getElementById("status");
+
+status.textContent = "Loading Face Tracking...";
+
+async function startFaceTracking() {
+    try {
+        const filesetResolver =
+            await FilesetResolver.forVisionTasks(
+                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+            );
+
+        status.textContent = "Loading Face Model...";
+
+        const faceLandmarker =
+            await FaceLandmarker.createFromOptions(
+                filesetResolver,
+                {
+                    baseOptions: {
+                        modelAssetPath:
+                            new URL(
+                                "../models/face_landmarker.task",
+                                import.meta.url
+                            ).href
+                    },
+                    runningMode: "VIDEO",
+                    numFaces: 1
+                }
+            );
+
+        status.textContent = "FACE TRACKING READY ✓";
+
+        function detectFace() {
+            if (video.readyState >= 2) {
+                const results =
+                    faceLandmarker.detectForVideo(
+                        video,
+                        performance.now()
+                    );
+
+                if (results.faceLandmarks.length > 0) {
+                    status.textContent = "FACE DETECTED ✓";
+                } else {
+                    status.textContent = "FACE NOT DETECTED";
+                }
+            }
+
+            requestAnimationFrame(detectFace);
+        }
+
+        detectFace();
+
+    } catch (error) {
+        console.error(error);
+        status.textContent = "ERROR: " + error.message;
+    }
+}
+
+startFaceTracking();
