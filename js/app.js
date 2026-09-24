1aa8256bf005f3087af249fe6754c2d05d784d19
rukhiya12345786-cpu <rukhiya12345786@gmail.com>
2026-09-24 16:23:10 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,3 +10,22 @@ const status = document.getElementById("status");
 
 console.log("Video:", video ? "FOUND" : "NOT FOUND");
 console.log("Status:", status ? "FOUND" : "NOT FOUND");
+const filesetResolver =
+    await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm"
+    );
+
+const faceLandmarker =
+    await FaceLandmarker.createFromOptions(
+        filesetResolver,
+        {
+            baseOptions: {
+                modelAssetPath: "../models/face_landmarker.task"
+            },
+            runningMode: "VIDEO",
+            numFaces: 1
+        }
+    );
+
+console.log("FACE LANDMARKER READY ✓");
+status.textContent = "Face Tracking Ready ✓";
