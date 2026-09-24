8759bbb6d1b3a195cefc83a066132d119f90de68
rukhiya12345786-cpu <rukhiya12345786@gmail.com>
2026-09-24 16:48:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+import {
+    FaceLandmarker,
+    FilesetResolver
+} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/vision_bundle.mjs";
 
+alert("APP.JS WORKING");
+
+const video = document.getElementById("camera");
+const status = document.getElementById("status");
+
+status.textContent = "APP.JS WORKING";
